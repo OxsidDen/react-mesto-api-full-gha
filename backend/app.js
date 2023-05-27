499d65b302ccb0078087ef1952256d3f6677fbc2
Denis Hazheev <denishazheev@MacBook-Pro-Denis.local>
2023-05-27 04:28:37 +0300

    initall 4.1

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const routes = require('./routes/routes');
 const { errorMiddlewares } = require('./middlewares/errors');
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT = 3109 } = process.env;
 
 mogoose.connect('mongodb://localhost:27017/mestodb');
 
