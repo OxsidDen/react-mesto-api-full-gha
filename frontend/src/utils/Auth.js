6ed425c77246a61f04e1ad35b06f2f58c3ad86ab
Denis Hazheev <denishazheev@MacBook-Pro-Denis.local>
2023-05-29 02:19:01 +0300

    initall 6

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -4,10 +4,12 @@ export const register = (email, password) => {
     return fetch(`${BASE_URL}/signup`, {
         method: 'POST',
         headers: {
-        'Accept': 'application/json',
         'Content-Type': 'application/json'
         },
-        body: JSON.stringify({email, password})
+        body: JSON.stringify({
+            email: email, 
+            password: password
+        })
     })
     .then((res) =>{
         if (res.ok) {
@@ -23,7 +25,6 @@ export const authorize = (email, password) => {
     return fetch(`${BASE_URL}/signin`, {
         method: 'POST',
         headers: {
-        'Accept': 'application/json',
         'Content-Type': 'application/json'
         },
         body: JSON.stringify({email, password})
@@ -46,7 +47,6 @@ export const checkToken = (token) => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
-        'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`,
         }
