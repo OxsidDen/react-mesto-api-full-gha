520617c683f27fc358e4cab4abeeb168b7a53bb1
Denis Hazheev <denishazheev@MacBook-Pro-Denis.local>
2023-07-07 00:17:33 +0300

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const mogoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -8,19 +7,18 @@ const errorCelebrate = require('celebrate').errors;
 const routes = require('./routes/routes');
 const { errorMiddlewares } = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const corsHandler = require('./middlewares/corsHandler');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 mogoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.use(cors({
-  origin: ['https://oxsid.nomoredomains.rocks', 'https://api.oxsid.nomoredomains.rocks', 'https://localhost:3000'],
-}));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(corsHandler);
 app.use(requestLogger);
 app.get('/crash-test', () => {
   setTimeout(() => {
